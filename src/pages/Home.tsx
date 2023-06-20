f26265f044b57f24f1a45e3f9b5c291b46d29a06
WEIDEMANN HAEFFELE Adrien <adrien.weidemann@gmail.com>
2023-06-20 22:02:30 +0200

    feat(home): init page welcome text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,19 +13,19 @@ export const Home = (): JSX.Element => {
   const { t } = useTranslation();
   const [ratingGrids, setRatingGrids] = useState<RatingGrid[]>([]);
   const [message, setMessage] = useState<string>("");
+  const [welcomeTextIndex, setWelcomeTextIndex] = useState<number>(0);
   const welcomeText: string = t("PAGE.HOME.WELCOME_TEXT");
-
-  //const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-  /*const setMessageTick = async () => {
-    await setMessage((previous: string): string => `${previous}${character}`);
-  };*/
+  const welcomeTextSplitted: string[] = welcomeText.split("");
 
   useEffect(() => {
-    welcomeText.split("").map((character: string) => {
-      setTimeout(function () {
-        setMessage((previous: string): string => `${previous}${character}`);
-      }, 1000);
-    });
+    const timer = setInterval(() => {
+      if (welcomeTextIndex === welcomeTextSplitted.length) {
+        clearInterval(timer);
+      } else {
+        setMessage((previous: string): string => previous + welcomeTextSplitted[welcomeTextIndex]);
+        setWelcomeTextIndex((previous: number): number => previous++);
+      }
+    }, 500);
 
     fetchRatingGrids()
       .then((ratingGrids: GetAll<RatingGrid>) => {
@@ -34,12 +34,12 @@ export const Home = (): JSX.Element => {
       .catch((err) => {
         console.error(err);
       });
-  }, [welcomeText]);
+  }, [welcomeTextIndex, welcomeTextSplitted]);
 
   return (
     <MainContainer>
       <div className="w-full text-left py-5 md:py-10">
-        <div className="group w-full p-5 text-gray-800 dark:text-gray-100 border-b border-black/10 bg-gray-50">
+        <div className="group w-full p-5 text-gray-800 border-b border-black/10 bg-gray-50">
           <span className="w-[30px]">
             <img height={22} width={22} className="inline" src={listLogo} alt="List logo" />
           </span>
