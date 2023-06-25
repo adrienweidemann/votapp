37af3787ef5f4d8cdef486c68779e4dbf194a701
Adrien <adrien.weidemann@gmail.com>
2023-06-26 00:31:33 +0200

    init serv

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
 
 var app = express();
 var directory = "/dist";
-app.use(express.static(__dirname + directory));
+app.use(express.static(path.dirname(__filename) + directory));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
