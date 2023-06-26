fb65937120f49987bb36193a529f159ba07b9aa7
Adrien <adrien.weidemann@gmail.com>
2023-06-26 07:54:42 +0200

    init serv

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const __filename = fileURLToPath(import.meta.url);
 
 var app = express();
 var directory = "/dist";
+
 app.use(express.static(path.dirname(__filename) + directory));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(path.dirname(__filename), "dist", "index.html"));
+app.get("*", function (req, res) {
+  res.sendFile("index.html", { root: path.join(path.dirname(__filename), directory) });
 });
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
